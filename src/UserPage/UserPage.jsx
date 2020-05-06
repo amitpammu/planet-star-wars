9163463cf10022fe01c7d88b8eac292f15d035a5
Amit kumar <amitkumar.d@altencalsoftlabs.com>
2020-05-06 13:58:21 +0530

    line break production issue

--- src/UserPage/UserPage.jsx
+++ src/UserPage/UserPage.jsx
@@ -4,6 +4,7 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -17,6 +18,7 @@ const UserPage = () => {
                 <Box bgcolor="error.main" color="info.contrastText" p={4}>
                     <Typography variant="h4" component="h2">
                         Welcome, {user && user.name}
+                    <Divider />
                         <Button variant="contained">
                             <Link color="textSecondary" to="/login">Logout</Link>
                         </Button>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
     API_CALL_LIMIT: 15,
-    ALLOWED_USER: "luke JSkywalker",
+    ALLOWED_USER: "luke Skywalker",
     ALLOWED_TIME: 1
 };
